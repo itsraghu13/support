9437e185ac3c3dc85bc68c87a69a624d49c64718
Raghavendra rao <73646752+itsraghu13@users.noreply.github.com>
2023-08-13 12:00:09 +0530

    Update conflict.py

--- conflict.py
+++ conflict.py
@@ -50,3 +50,89 @@ if len(run_list) > 0:
 else:
     raise Exception('get_pipeline_list returned empty')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import concurrent.futures
+import delta
+
+def process_run(item):
+    queryActivity_URL = "https://management.azure.com/subscriptions/{}/resourceGroups/{}/providers/"
+    headers = {'Authorization': 'Bearer ' + access_token}
+
+    # Get pipeline run details for the given pipelineName and run id
+    pipeline_runs = fetch_pipeline_runs(queryActivity_URL, headers, generate_request_body(last_updated_after, last_updated_before, False))
+    pipeline_runs = pipeline_runs['value']
+
+    if pipeline_runs is not None and len(pipeline_runs) > 0:
+        result = get_activity_output_data(pipeline_runs, item['runid'])
+        parent_id, parent_name = get_parent_id_recursive(item['runid'])[0:2]
+
+        df_parameters_data = get_parameters_data(item['runid'])
+
+        table_exists = check_table_exists("structured.t_meta_copyactivities")
+
+        if result:
+            df = spark.createDataFrame(result, schema)
+
+            if parent_id:
+                df = df.withColumn("parent_runid", lit(parent_id)).withColumn("parent_name", lit(parent_name)).withColumn("requested_date", lit(current_date()))
+            else:
+                df = df.withColumn("parent_runid", lit('NA')).withColumn("parent_name", lit('NA')).withColumn("requested_date", lit(current_date()))
+
+            if table_exists:
+                while True:
+                    try:
+                        spark.sql("BEGIN TRANSACTION")
+                        df.createOrReplaceTempView("temp_copyactivities")
+                        sql_query_upsert = """
+                            MERGE INTO structured.t_meta_copyactivities_123
+                            AS target USING temp_copyactivities AS source
+                            ON target.activityrunid = source.activityrunid
+                            """
+                        spark.sql(sql_query_upsert)
+                        spark.sql("COMMIT")
+                        break  # Exit the loop on successful commit
+                    except DeltaConcurrencyControlException as e:
+                        # Conflict detected, refresh data and try again
+                        df = refresh_df(df)
+        else:
+            pass
+    else:
+        raise Exception('get_pipeline_runs did not return anything')
+
+if len(run_list) > 0:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        # You can adjust max_workers to control the number of parallel threads
+        futures = [executor.submit(process_run, item) for item in run_list]
+
+        # Wait for all threads to finish
+        concurrent.futures.wait(futures)
+
+else:
+    raise Exception('get_pipeline_list returned empty')
+
+
