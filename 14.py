4d4d376713053dfc35a3724222744665e5d5f655
Raghavendra rao <73646752+itsraghu13@users.noreply.github.com>
2023-08-14 15:51:16 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,55 +1,47 @@
-import delta
-import time
+# Initialize an empty list to store the collected data
+collected_data = []
 
-def resolve_conflicts_optimistically(table_path):
-    """
-    Resolves conflicts in the given Delta Lake table optimistically.
+if len(run_list) > 0:
+    for item in run_list:
+        queryActivity_URL = "https://management.azure.com/subscriptions/{}/resourceGroups/{}/providers/"
+        headers = {'Authorization': 'Bearer ' + access_token}
+        
+        pipeline_runs = fetch_pipeline_runs(queryActivity_URL, headers, generate_request_body(last_updated_after, last_updated_before, False))
+        pipeline_runs = pipeline_runs['value']
+        
+        if pipeline_runs is not None and len(pipeline_runs) > 0:
+            result = get_activity_output_data(pipeline_runs, item['runid'])
+            parent_id, parent_name = get_parent_id_recursive(item['runid'])[0:2]
+            
+            df_parameters_data = get_parameters_data(item['runid'])
+            
+            if result:
+                collected_data.append((result, parent_id, parent_name, current_date()))
+            else:
+                pass
+        else:
+            raise Exception('get_pipeline_runs did not return anything')
+else:
+    raise Exception('get_pipeline_list returned empty')
 
-    Args:
-        table_path: The path to the Delta Lake table.
-    """
-
-    # Get the current table version.
-    current_version = delta.get_current_version(table_path)
-
-    # Read the delta log.
-    delta_log = delta.read_delta_log(table_path)
-
-    # Find the latest committed version that is before the current version.
-    latest_committed_version = None
-    for entry in delta_log.entries:
-        if entry.version <= current_version and latest_committed_version is None:
-            latest_committed_version = entry.version
-
-    # If there is no latest committed version, then there are no conflicts.
-    if latest_committed_version is None:
-        return
-
-    # Get the reads and writes for the current transaction.
-    reads = []
-    writes = []
-
-    # Attempt a commit.
-    commit = delta.commit(table_path, reads, writes)
-
-    # If the commit fails, then there is a conflict.
-    if commit.state != delta.DeltaCommitState.COMMITTED:
-        # Check the delta log to see if the conflicting data has been committed since the start of the current transaction.
-        for entry in delta_log.entries:
-            if entry.version > latest_committed_version and entry.version <= commit.version:
-                # The conflicting data has been committed, so we need to abort the transaction.
-                raise delta.DeltaLakeException("Conflict detected")
-
-        # The conflicting data has not been committed, so we can retry the commit.
-        print("Conflict detected, retrying...")
-        time.sleep(1)
-        resolve_conflicts_optimistically(table_path)
-
-    # The commit was successful, so there are no conflicts.
-    print("Commit successful")
-    return commit
-
-
-if __name__ == "__main__":
-    table_path = "/path/to/table"
-    resolve_conflicts_optimistically(table_path)
+# After collecting data from all runids, create a DataFrame and insert into the table
+if collected_data:
+    data_rows = []
+    for data in collected_data:
+        result, parent_id, parent_name, requested_date = data
+        for row in result:
+            data_rows.append(row + (parent_id, parent_name, requested_date))
+    
+    collected_df = spark.createDataFrame(data_rows, schema)
+    
+    table_exists = check_table_exists("structured.t_meta_copyactivities")
+    if table_exists:
+        collected_df.createOrReplaceTempView("temp_copyactivities")
+        sql_query_upsert = """
+            MERGE INTO structured.t_meta_copyactivities_123
+            AS target USING temp_copyactivities AS source
+            ON target.activityrunid = source.activityrunid
+            """
+        spark.sql(sql_query_upsert)
+    else:
+        collected_df.write.format("delta").mode("append").option("overwriteSchema", "true").saveAsTable("structured.t_meta_copyactivities")
