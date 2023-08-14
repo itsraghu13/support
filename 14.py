1317b9e0085684e938e3094d071fa1ba7f6c88a1
Raghavendra rao <73646752+itsraghu13@users.noreply.github.com>
2023-08-14 16:13:37 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -103,3 +103,44 @@ if collected_data:
     else:
         collected_df.write.format("delta").mode("append").option("overwriteSchema", "true").saveAsTable("structured.t_meta_copyactivities")
 
+
+
+
+
+
+
+
+
+
+
+if collected_data:
+    rows = []
+    for data in collected_data:
+        result, parent_id, parent_name, requested_date = data
+        for row_dict in result:
+            row_dict["parent_id"] = parent_id
+            row_dict["parent_name"] = parent_name
+            row_dict["requested_date"] = requested_date
+            rows.append(tuple(row_dict.values()))
+    
+    # Create a temporary table to hold the rows
+    temp_table_name = "temp_copyactivities_table"
+    temp_table_df = spark.createDataFrame(rows, schema)
+    temp_table_df.createOrReplaceTempView(temp_table_name)
+    
+    # Perform the merge operation using SQL
+    table_exists = check_table_exists("structured.t_meta_copyactivities")
+    if table_exists:
+        sql_query_upsert = f"""
+            MERGE INTO structured.t_meta_copyactivities_123
+            AS target USING {temp_table_name} AS source
+            ON target.activityrunid = source.activityrunid
+            """
+        spark.sql(sql_query_upsert)
+    else:
+        temp_table_df.write.format("delta").mode("append").option("overwriteSchema", "true").saveAsTable("structured.t_meta_copyactivities")
+    
+    # Drop the temporary table
+    spark.catalog.dropTempView(temp_table_name)
+
+
